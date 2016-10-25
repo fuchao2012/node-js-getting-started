9a7e350b31204b3d978f8ae6bf2799b849d698e7
郑付超 <fuchao.zheng@ikang.com>
2016-10-25 11:48:59 +0800

    update to map

--- routes/maps.js
+++ routes/maps.js
@@ -2,20 +2,20 @@
 var router = require('express').Router();
 var AV = require('leanengine');
 
-var UnsignedSite = AV.Object.extend('data');
+var UnsignedSite = AV.Object.extend('siteinfo');
 router.get('/',function(req,res,next){
     var query = new AV.Query(UnsignedSite);
     query.descending('createdAt');
     query.startsWith('country',req.query.content || '');
     console.log('\n\n search',req.query.content || '')
     query.find().then(function(results){
-        res.render('todos',{
+        res.render('maps',{
             sites: results
         });
     },function(err){
       console.log(err);
         if (err.code === 101) {
-      res.render('todos', {
+      res.render('maps', {
         sites: []
       });
     } else {
